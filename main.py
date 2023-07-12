6fca34181376168a3348c30f4fc9138bbe48ad3e
laluxx <lorenzoaddamo5@gmail.com>
2023-07-12 10:37:51 +0000

    added-samples

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
-import random
 import pygame
 from pynput import keyboard
+import random
 
 # Get the directory of this script
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 # Path to the theme directory. Change 'theme_name' to your desired theme.
-theme_dir = os.path.join(script_dir, "switches", "silver")
+theme_dir = os.path.join(script_dir, "switches", "bloop")
 
 # Initialize Pygame
 pygame.init()
@@ -22,27 +22,26 @@ for sound_file in os.listdir(theme_dir):
         sound_path = os.path.join(theme_dir, sound_file)
         sounds[sound_file] = pygame.mixer.Sound(sound_path)
 
-# Define a function that plays the sound when a key is pressed
+# Define a mapping from key combinations to sounds
+key_to_sound = {
+    (keyboard.Key.cmd, keyboard.KeyCode(char='1')): 'key-press-1.wav',
+    (keyboard.Key.cmd, keyboard.KeyCode(char='2')): 'key-press-2.wav',
+    # Add more key combinations as needed...
+}
+
 def on_press(key):
     try:
         key_name = key.char
     except AttributeError:
         key_name = key.name.lower()  # Convert to lowercase for case-insensitive comparison
 
-    # Do not play sounds for the specified keys
-    excluded_keys = ["h", "j", "k", "l", "up", "right", "left", "down", "1", "2", "3", "4", "5", "6", "backspace", "enter", "shift", "ctrl", "cmd", "alt", "tab"]
-    excluded_combinations = [(keyboard.Key.shift, keyboard.Key.cmd, keyboard.KeyCode(char='j')),
-                             (keyboard.Key.shift, keyboard.Key.cmd, keyboard.KeyCode(char='h'))]
-
-    if key_name in excluded_keys:
-        # Check for excluded combinations and return if present
-        for comb in excluded_combinations:
-            if key == comb:
-                return
-        else:
+    # Check for specific key combinations and play the corresponding sound
+    for comb, sound_name in key_to_sound.items():
+        if key == comb and sound_name in sounds:
+            sounds[sound_name].play()
             return
 
-    # Play a random sound for any key
+    # Play a random sound for any other key
     sound = random.choice(list(sounds.values()))
     sound.play()
 
@@ -50,123 +49,3 @@ def on_press(key):
 listener = keyboard.Listener(on_press=on_press)
 listener.start()
 listener.join()
-
-
-
-
-#0.0.1
-
-# import os
-# import random
-# import pygame
-# from pynput import keyboard
-#
-# # Get the directory of this script
-# script_dir = os.path.dirname(os.path.realpath(__file__))
-#
-# # Path to the theme directory. Change 'theme_name' to your desired theme.
-# theme_dir = os.path.join(script_dir, "switches", "silver")
-#
-# # Initialize Pygame
-# pygame.init()
-#
-# # Initialize Pygame mixer
-# pygame.mixer.init()
-#
-# # Dynamically load all sounds into a dictionary
-# sounds = {}
-# for sound_file in os.listdir(theme_dir):
-#     if sound_file.endswith(".ogg"):
-#         sound_path = os.path.join(theme_dir, sound_file)
-#         sounds[sound_file] = pygame.mixer.Sound(sound_path)
-#
-# # Define a function that plays the sound when a key is pressed
-# def on_press(key):
-#     try:
-#         key_name = key.char
-#     except AttributeError:
-#         key_name = key.name.lower()  # Convert to lowercase for case-insensitive comparison
-#
-#     # Do not play sounds for the specified keys
-#     excluded_keys = ["h", "j", "k", "l", "up", "right", "left", "down", "1", "2", "3", "4", "5", "6", "backspace", "enter", "shift", "ctrl", "cmd", "alt", "tab"]
-#     excluded_combinations = [(keyboard.Key.shift, keyboard.Key.cmd, keyboard.KeyCode(char='j')),
-#                              (keyboard.Key.shift, keyboard.Key.cmd, keyboard.KeyCode(char='h'))]
-#
-#     if key_name in excluded_keys:
-#         # Check for excluded combinations and return if present
-#         for comb in excluded_combinations:
-#             if key == comb:
-#                 return
-#         else:
-#             return
-#
-#     # Play a random sound for any key
-#     sound = random.choice(list(sounds.values()))
-#     sound.play()
-#
-# # Set up the key listener
-# listener = keyboard.Listener(on_press=on_press)
-# listener.start()
-# listener.join()
-
-# 0.0.0
-
-# import os
-# import random
-# import pygame
-# from pynput import keyboard
-
-# # Change to the directory containing the sound files
-# os.chdir("/home/l/Desktop/xos/keyboard-sounds")
-
-# # Initialize Pygame
-# pygame.init()
-
-# # Initialize Pygame mixer
-# pygame.mixer.init()
-
-# # Load all sounds into a dictionary
-# sounds = {
-#     "a": pygame.mixer.Sound("key-press.ogg"),
-#     "b": pygame.mixer.Sound("key-press-1.ogg"),
-#     "c": pygame.mixer.Sound("key-press-2.ogg"),
-#     "d": pygame.mixer.Sound("key-press-3.ogg"),
-#     "e": pygame.mixer.Sound("key-press-4.ogg"),
-#     "backspace": pygame.mixer.Sound("key-delete.ogg"),
-#     "space": pygame.mixer.Sound("key-press-2.ogg"),
-# }
-
-# # Define a function that plays the sound when a key is pressed
-# def on_press(key):
-#     try:
-#         key_name = key.char
-#     except AttributeError:
-#         key_name = key.name.lower()  # Convert to lowercase for case-insensitive comparison
-
-#     # Do not play sounds for the specified keys
-#     excluded_keys = ["h", "j", "k", "l", "up","right", "left", "down", "1", "2", "3", "4", "5", "6", "backspace", "enter", "shift", "ctrl", "cmd", "alt", "tab"]
-#     excluded_combinations = [(keyboard.Key.shift, keyboard.Key.cmd, keyboard.KeyCode(char='j')),
-#                              (keyboard.Key.shift, keyboard.Key.cmd, keyboard.KeyCode(char='h'))]
-
-#     if key_name in excluded_keys:
-#         # Check for excluded combinations and return if present
-#         for comb in excluded_combinations:
-#             if key == comb:
-#                 return
-#         else:
-#             return
-
-#     # Play a random sound for any key other than spacebar
-#     if key_name != "space":
-#         sound = random.choice(list(sounds.values()))
-#         sound.play()
-
-#     # Play the corresponding sound for spacebar
-#     else:
-#         sound = sounds[key_name]
-#         sound.play()
-
-# # Set up the key listener
-# listener = keyboard.Listener(on_press=on_press)
-# listener.start()
-# listener.join()
---
+++ original.py
@@ -0,0 +1,52 @@
+import os
+import random
+import pygame
+from pynput import keyboard
+
+# Get the directory of this script
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Path to the theme directory. Change 'theme_name' to your desired theme.
+theme_dir = os.path.join(script_dir, "switches", "bloop")
+
+# Initialize Pygame
+pygame.init()
+
+# Initialize Pygame mixer
+pygame.mixer.init()
+
+# Dynamically load all sounds into a dictionary
+sounds = {}
+for sound_file in os.listdir(theme_dir):
+    if sound_file.endswith(".ogg") or sound_file.endswith(".wav"):
+        sound_path = os.path.join(theme_dir, sound_file)
+        sounds[sound_file] = pygame.mixer.Sound(sound_path)
+
+# Define a function that plays the sound when a key is pressed
+def on_press(key):
+    try:
+        key_name = key.char
+    except AttributeError:
+        key_name = key.name.lower()  # Convert to lowercase for case-insensitive comparison
+
+    # Do not play sounds for the specified keys
+    excluded_keys = ["h", "j", "k", "l", "up", "right", "left", "down", "1", "2", "3", "4", "5", "6", "backspace", "enter", "shift", "ctrl", "cmd", "alt", "tab"]
+    excluded_combinations = [(keyboard.Key.shift, keyboard.Key.cmd, keyboard.KeyCode(char='j')),
+                             (keyboard.Key.shift, keyboard.Key.cmd, keyboard.KeyCode(char='h'))]
+
+    if key_name in excluded_keys:
+        # Check for excluded combinations and return if present
+        for comb in excluded_combinations:
+            if key == comb:
+                return
+        else:
+            return
+
+    # Play a random sound for any key
+    sound = random.choice(list(sounds.values()))
+    sound.play()
+
+# Set up the key listener
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+listener.join()
